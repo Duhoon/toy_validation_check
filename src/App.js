cbea540c14f2bb98cb7c314903cd8676be990e38
Kimkideok1 <meviuswork@gmail.com>
2022-10-01 18:48:06 +0900

    singin 라우트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Signin from './Page/Signin';
 import Signup from './Page/Signup';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Signin/>
+      <Routes>
+        <Route path="./Page/Signin" element={<Signin/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/Page/Signin.js
+++ src/Page/Signin.js
@@ -1,14 +1,17 @@
 import React,{useState} from 'react';
 import './Signin.css';
+import {Link} from 'react-router-dom';
 
 function Signin(){
-    
+
     return(
         <div>
-            <form>
+                <Link to ="/">Login</Link>
+             <form>
                 <input className ="signin_userid" type="text"></input>
                 <input className ="signin_password" type="text"></input>
                 <button>Login</button>
+                <button><Link to="/Signup">회원가입</Link></button>                
             </form>
         </div>
     )    
