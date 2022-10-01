e804ae80f3e642b171adab4347e204fbb4a0b6d7
Duhoon <eclip6@naver.com>
2022-10-01 20:23:12 +0900

    signin userId input fetch

--- src/Page/Signin.js
+++ src/Page/Signin.js
@@ -4,27 +4,43 @@ import {Link} from 'react-router-dom';
 
 function Signin(){
 
-//1. username에 text(id) 값을 입력 받음
-//2. password에 text(passwd) 값을 받음
-//3. Login 버튼을 눌렀을 때 찾는데 유저의 정보가 저장이 안되어 있으면 회원가입
+    //1. userid에 text(id) 값을 입력 받음 < html
+        // 1-1. 컴포넌트 안에서 userId state를 만들고 저장
+        // 1-2. userId 길이가 0 보다 커야 유효성 통과
+    //2. password에 text(passwd) 값을 입력 받음 < html
+        // 2-1. 컴포넌트 안에서 userPasswd state를 만들고 저장
+        // 2-2. userPasswd 길이가 0 보다 커야 유효성 통과
+    //3. Login 버튼을 눌렀을 때 userId 에 유효성 통과가 됬고,
+    //   userPasswd가 유효성 통과가 됬으면 = > success
+    //   아니면 ? 입력해달라고 안내문구 출력
+
+
+// 유저의 정보가 저장이 안되어 있으면 회원가입
 //4. Login 버튼을 눌렀을 때 있으면 로그인 되었습니다.
 //5. 각 태그에 이벤트 핸들러로 고차함수 적용
-    let userId = [userId , setuserId] = useState("[]"); //ID 저장
-    let userPasswd = [userPass , setuserPass] = useState("[]"); //Pass 저장
 
-    function userId(el_id){
-        return 
+    // 어떤 관리를 하려고 하셨나요.
+    // 값이 하나하나 저장이 될것
+    // 스트링을 저장해서 가져오는 것보다 나아서.
+
+    const [userId , setuserId] = useState(""); //ID 저장
+    const [userPasswd , setuserPass] = useState(""); //Pass 저장
+
+    function handleId(e){
+        setuserId(e.target.value);
     }
 
-    function userPasswd(el_pass){
+    function handlePasswd(el_pass){
         return
     }
+
 
     return(
         <div>
              <form>
-                <input className ="signin_userid" type="text">{userId}</input>
-                <input className ="signin_password" type="text">{userPasswd}</input>
+                <input className ="signin_userid" type="text" onChange={handleId} value={userId}></input>
+                {userId.length < 1 ? <div>아이디를 입력해주세요</div> : <></>}
+                <input className ="signin_password" type="text" onChange={handlePasswd}></input>
                 <button>Login</button>
                 <button><Link to="/Signup">회원가입</Link></button>                
             </form>
--- src/Validation/validation.js
+++ src/Validation/validation.js
@@ -1,16 +1,34 @@
-/* 로그인 페이지 유효성 검사*/
 
 
-let signinIdCheck = (id)=>{
-    return id.length > 0 ? true : false;
+export default {
+    /* 로그인 페이지 유효성 검사*/
+
+    signinIdCheck : (id)=>{
+        return id.length > 0 ? true : false;
+    },
+
+    signinPasswordCheck : (password)=>{
+        return password.length > 0 ? true : false;
+    },
+
+    /* 회원가입 페이지 유효성 검사 */
+
+    signupIdCheck : (id)=>{
+        let checkRegExp = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\w{8,}/ ;
+        return checkRegExp.test(id);
+    },
+
+    signupPasswordCheck : (password)=>{
+        let checkRegExp = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}/
+        return checkRegExp.test(password);
+    },
+
+    signupEmailCheck : (email)=>{
+        let checkRegExp = /^[\w][\w]*@/
+    },
+
 }
 
-let signinPasswordCheck = (password)=>{
-    return password.length > 0 ? true : false;
-}
-
-/* 회원가입 페이지 유효성 검사 */
-
-let signupIdCheck = (id)=>{
-    return 
-}
+// userId 함수 -> 입력받는 텍스트 값을 입력을 받으면 그것을 배열에 넣고
+// 순서도?
+// 두 함수에 어떻게 입력이 들어올까요? 이벤트에서 넣어야 된다고요?
